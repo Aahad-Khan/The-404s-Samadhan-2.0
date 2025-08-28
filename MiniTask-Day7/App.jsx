f050c270736e4af0affb8dfe392a14488dca0850
Mayank Bamne <mayankbamne6@gmail.com>
2025-08-28 23:14:45 +0530

    Add files via upload

---
+++ MiniTask-Day7/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from "react";
+import Counter from "./Counter";  // Import the Counter component
+
+function App() {
+  return (
+    <div>
+      <h1>State Management with Counter</h1>
+      <Counter />  {/* Render the Counter component */}
+    </div>
+  );
+}
+
+export default App;
