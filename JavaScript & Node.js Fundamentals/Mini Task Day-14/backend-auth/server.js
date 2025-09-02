a78c8f7c8869378a636b666115efc4abd3b2b674
harsh7828 <140946375+harsh7828@users.noreply.github.com>
2025-09-03 00:04:58 +0530

    Add files via upload

---
+++ JavaScript & Node.js Fundamentals/Mini Task Day-14/backend-auth/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
